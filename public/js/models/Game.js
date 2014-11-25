451753c43deb33553ec46f3ec4f50b32321b2215
Isaac Noda <isaacnoda@gmail.com>
2014-11-24 20:24:58 -0600

    Changed scoring system

--- public/js/models/Game.js
+++ public/js/models/Game.js
@@ -2,7 +2,7 @@ function Game() {
   this.board = new Board();
   this.boardView = new BoardView(this.board);
   this.minimaxScoresAndMoves = {};
-  this.minimaxMaxDepth = 4;
+  this.minimaxMaxDepth = 7;
 }
 
 Game.prototype.display = function() {
@@ -15,7 +15,7 @@ Game.prototype.chooseBestMove = function() {
   var bestScore, bestMove, move, score;
 
   this.getMinimaxScores(this.minimaxMaxDepth, "O", this.board);
-
+  console.log(this.minimaxScoresAndMoves)
   bestScore = -1000;
   bestMove = -1;
 
@@ -69,7 +69,7 @@ Game.prototype.getMinimaxScores = function(depth, player, board) {
 
   // If the board is full, there's a line with three in a row (a winner), or depth is 0, we've reached the end of this 'branch'
   if (board.isFull() || board.hasThreeInARow() || depth === 0) { 
-    return evaluateBoard(board.layout);
+    return evaluateAndScoreBoard(board, depth);
   }
 
   // Clones the passed in board and board layout so that the original board isn't changed
@@ -115,79 +115,12 @@ Game.prototype.getMinimaxScores = function(depth, player, board) {
 }
 
 
-
-// Gives a score to the current state of the board
-function evaluateBoard(board) {
-  var score = 0;
-
-  score += evaluateLine(board, 0, 1, 2);
-  score += evaluateLine(board, 3, 4, 5);
-  score += evaluateLine(board, 6, 7, 8);
-  score += evaluateLine(board, 0, 3, 6);
-  score += evaluateLine(board, 1, 4, 7);
-  score += evaluateLine(board, 2, 5, 8);
-  score += evaluateLine(board, 0, 4, 8);
-  score += evaluateLine(board, 2, 4, 6);
-
-  return score;
-}
-
-// Gives each line a score from the AI's perspective (more points for lines that are
-// better for O)
-function evaluateLine(board, index1, index2, index3) {
-  // If the line is empty or the line looks like XXO or OOX, score = 0
-  // If the line has 1 O and nothing else, score = 1
-  // If the line has 1 X and nothing else, score = -1
-  // If the line looks like OXO, score = 1
-  // If the line looks like XOX, score = -1
-  // If the line has 2 O's and nothing else, score = 10
-  // If the line has 2 X's and nothing else, score = -10
-  // If the line has 3 O's (AI wins), score = 100
-  // If the line has 3 X's (player wins), score = -100
-
-  var score = 0;
-
-  if (board[index1] === 'O') {
-    score = 1;
-  } else if (board[index1] === 'X') {
-    score = -1;
+function evaluateAndScoreBoard(board, depth) {
+  if (board.threeInARowCharacter() == 'X') {
+    return -100 * depth
+  } else if (board.threeInARowCharacter() == 'O') {
+    return 100 * depth
+  } else {
+    return 0
   }
-
-  if (board[index2] === 'O') {
-    if (score === 1) {
-      score = 10;
-    } else if (score === -1) {
-      return 0;
-    } else {
-      score = 1;
-    }
-  } else if (board[index2] === 'X') {
-    if (score === -1) {
-      score = -10;
-    } else if (score === 1) {
-      return 0;
-    } else {
-      score = -1;
-    }
-  }
-
-  if (board[index3] === 'O') {
-    if (score > 0) {
-      score *= 10;
-    } else if (score < 0) {
-      return 0;
-    } else {
-      score = 1;
-    }
-  } else if (board[index3] === 'X') {
-    if (score < 0) {
-      score *= 10;
-    } else if (score > 0) {
-      return 0;
-    } else {
-      score = -1;
-    }
-  }
-
-  return score;
 }
